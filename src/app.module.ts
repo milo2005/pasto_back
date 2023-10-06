c7fa4bfe2e934ba1f9db24dda19808e62159c34f
Darion Chamorro <dario.chamorro.v@gmail.com>
2023-10-05 20:04:02 -0500

    Mongoose configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { PlansModule } from './plans/plans.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [EventsModule, PlansModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot("mongodb://localhost/pastohoy"),
+    EventsModule, 
+    PlansModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { EventAdapter } from './data/adapters/event.adapter';
+import { EventDao } from './data/dao/event.dao';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EventSchema, Event } from './data/schemas/event.schema';
+
 
 @Module({
-  providers: [EventsService],
+  imports: [
+    MongooseModule.forFeature([
+      {name: Event.name, schema: EventSchema}
+    ])
+  ],
+  providers: [
+    EventDao,
+    EventAdapter,
+    EventsService
+  ],
   controllers: [EventsController]
 })
 export class EventsModule {}
